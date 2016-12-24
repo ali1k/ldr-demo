d0f724ce20684f380f09689441b31b4fe094f1db
ali1k <hyperir@gmail.com>
2016-12-24 23:24:32 +0330

    removed h2020 facets config

--- configs/facets.js
+++ configs/facets.js
@@ -117,36 +117,6 @@ export default {
                 }
             }
         },
-        'http://risis.eu/cordisH2020': {
-            list: [
-                'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-                'http://risis.eu/cordisH2020/vocab/projectParticipant',
-                'http://risis.eu/cordisH2020/vocab/fundingScheme',
-                'http://risis.eu/cordisH2020/vocab/topic',
-                'http://risis.eu/cordisH2020/vocab/totalCost',
-                'http://risis.eu/cordisH2020/vocab/callID',
-                'http://rdf-vocabulary.ddialliance.org/discovery#startDate',
-                'http://risis.eu/cordisH2020/vocab/durationMonths'
-            ],
-            config: {
-                'http://www.w3.org/1999/02/22-rdf-syntax-ns#type': {
-                    label: ['Type'],
-                    hint: ['Type of the resource under investigation.']
-                },
-                'http://risis.eu/cordisH2020/vocab/projectParticipant': {
-                    label: ['Participant'],
-                    hasLinkedValue: 1
-                },
-                'http://risis.eu/cordisH2020/vocab/fundingScheme': {
-                    label: ['Funding Scheme'],
-                    hasLinkedValue: 1
-                },
-                'http://risis.eu/cordisH2020/vocab/topic': {
-                    label: ['Topic'],
-                    hasLinkedValue: 1
-                }
-            }
-        },
         //Configuration Manager: change the graph name if you use another name in your general.js config
         'http://ld-r.org/configurations': {
             list: [
